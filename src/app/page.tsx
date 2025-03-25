b81dab9c92c91f54cb62eabe8d67cd27a2dc4f83
John Doe <johndoe@example.com>
2025-03-25 18:51:49 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Header from "@/components/Header/Header";
+
 import Hero from "@/components/Hero/Hero";
 import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
 import BenefitsSlider from "@/components/BenefitsSlider/BenefitsSlider";
