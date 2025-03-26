8b1a9ca62e54efa8e3e16345aef96d13f86add7d
John Doe <johndoe@example.com>
2025-03-26 10:21:31 +0200

    add catalog

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { products, Product } from './productsData';
 import styles from './Catalog.module.css';
 
@@ -33,7 +33,6 @@ const Catalog: React.FC = () => {
   };
 
   return (
-    //@ts-ignore
     <section className={styles.catalogSection}>
       <div className={styles.sectionHeader}>
         <h2 className={styles.title}>Икорный Бутик</h2>
